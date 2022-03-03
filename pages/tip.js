f14ee57c63cfaf3c0c1c630e2ddd72ff3d421cf4
Ameno-ab <99277062+Ameno-ab@users.noreply.github.com>
2022-03-03 21:43:25 +0300

    enter

--- pages/tip.js
+++ pages/tip.js
@@ -1,78 +1,152 @@
-const Tip =()=>{
+import { useState, UseContext, useContext } from "react";
+const Tip = () => {
+  const [cal, setCal] = useState("");
 
-    return(
+  const [bill, setBill] = useState("");
+  const [per,setPer]= useState("1");
 
-        <div>
-  <meta charSet="UTF-8" />
-  <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-  <link href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@700&display=swap" rel="stylesheet" />
-  <link rel="stylesheet" href="./styles/styles.css" />
-  <title>tip calculator </title>
-  <div className="container">
-    <div className="logo">
-      <img src="../images/logo.svg" alt="logo" />
-    </div>
-    <div className="card">
-      <div className="input">
-        <div className="bill-container">
-          <div className="label-wrapper">
-            <label htmlFor="inp-bill">Bill</label>
-          </div>
-          <div className="inp-wrapper">
-            <input className="inp-text" type="text" name="inp-bill" id="inp-bill" placeholder={0.0} />
-            <img src="../images/icon-dollar.svg" alt="dollar.icon" />
-          </div>
+  const calculator = (sum) => {
+    sum = (cal / 100) * bill;
+    return sum;
+  };
+  const total = (sum) => {
+    sum = (cal / 100) * bill * per;
+    return sum;
+  };
+  return (
+    <div>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@700&display=swap"
+        rel="stylesheet"
+      />
+      <link rel="stylesheet" href="./styles/styles.css" />
+      <title>tip calculator </title>
+      <div className="container">
+        <div className="logo">
+          <img src="../images/logo.svg" alt="logo" />
         </div>
-        <div className="tip-container">
-          <div className="label-wrapper">
-            <label htmlFor="inp-tip">Select tip%</label>
+        <div className="card">
+          <div className="input">
+            <div className="bill-container">
+              <div className="label-wrapper">
+                <label htmlFor="inp-bill">Bill</label>
+              </div>
+              <div className="inp-wrapper">
+                <input
+                  className="inp-text"
+                  value={bill}
+                  onChange={(e) => setBill(e.target.value)}
+                  type="text"
+                  placeholder={0.0}
+                />
+
+                <img src="../images/icon-dollar.svg" alt="dollar.icon" />
+              </div>
+            </div>
+            <div className="tip-container">
+              <div className="label-wrapper">
+                <label htmlFor="inp-tip">Select tip%</label>
+              </div>
+              <div className="btn-wrapper">
+                <button
+                  className="btn"
+                  value={cal}
+                  onClick={(e) => setCal(5)}
+                  type="number"
+                >
+                  5%
+                </button>
+                <button
+                  className="btn"
+                  value={cal}
+                  onClick={(e) => setCal(10)}
+                  type="number"
+                >
+                  10%
+                </button>
+                <button
+                  className="btn"
+                  value={cal}
+                  onClick={(e) => setCal(15)}
+                  type="number"
+                >
+                  15%
+                </button>
+                <button
+                  className="btn"
+                  value={cal}
+                  onClick={(e) => setCal(25)}
+                  type="number"
+                >
+                  25%
+                </button>
+                <button
+                  className="btn"
+                  value={cal}
+                  onClick={(e) => setCal(50)}
+                  type="number"
+                >
+                  50%
+                </button>
+                <input
+                  className="inp-text"
+                  type="text"
+                  name="inp-tip"
+                  id="inp-tip"
+                  placeholder="custom"
+                />
+              </div>
+            </div>
+            <div className="people-container">
+              <div className="label-wrapper">
+                <label htmlFor="inp-people">number of people</label>
+                <div className="error-msg">can't be zero</div>
+              </div>
+              <div className="inp-wrapper">
+              <input
+                  className="inp-text"
+                  value={per}
+                  onChange={(e) => setPer(e.target.value)}
+                  type="text"
+                  placeholder={1}
+                />
+
+                <img src="../images/icon-person.svg" alt="icon-person.icon" />
+              </div>
+            </div>
           </div>
-          <div className="btn-wrapper">
-            <div className="btn">5%</div>
-            <div className="btn">10%</div>
-            <div className="btn btn-active">15%</div>
-            <div className="btn">25%</div>
-            <div className="btn">50%</div>
-            <input className="inp-text" type="text" name="inp-tip" id="inp-tip" placeholder="custom" />
-          </div>
-        </div>
-        <div className="people-container">
-          <div className="label-wrapper">
-            <label htmlFor="inp-people">number of people</label>
-            <div className="error-msg">can't be zero</div>
-          </div>
-          <div className="inp-wrapper">
-            <input className="inp-text" type="text" name="inp-people" id="inp-people" placeholder={1} />
-            <img src="../images/icon-person.svg" alt="icon-person.icon" />
+          <div className="output">
+            <div className="line-wrapper">
+              <div className="line-output">
+                <div className="title-wrapper">
+                  <div className="title">Tip amount </div>
+                  <div className="decs">/person</div>
+                </div>
+
+                <div class="value"> {calculator()}</div>
+              </div>
+              <div className="line-output">
+                <div className="title-wrapper">
+                  <div className="title">Total</div>
+                  <div className="decs">/person</div>
+                </div>
+                <div className="value"> {total()}</div>
+              </div>
+            </div>
+            <div className="btn"
+             className="btn"
+             value={cal}
+             onClick={(e) => setCal("")}
+             type="number">Reset</div>
           </div>
         </div>
       </div>
-      <div className="output">
-        <div className="line-wrapper">
-          <div className="line-output">
-            <div className="title-wrapper">
-              <div className="title">Tip amount </div>
-              <div className="decs">/person</div>
-            </div>
-            <div className="value">$4.27 </div>
-          </div>
-          <div className="line-output">
-            <div className="title-wrapper">
-              <div className="title">Total</div>
-              <div className="decs">/person</div>
-            </div>
-            <div className="value"> $32.27</div>
-          </div>
-        </div>
-        <div className="btn">Reset</div>
-      </div>
     </div>
-  </div>
-</div>
- 
-    );
-}
+  );
+};
 export default Tip;
